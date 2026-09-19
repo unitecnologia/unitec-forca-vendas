77d35181972f9fca42fd5d134a8b6e91fd634148
unitecnologia <alencar@unitecnologiasc.com.br>
2026-09-19 10:02:52 -0300

    chore: nome do APK Unitec-Forca-Vendas-{version} no build release

--- ci/build.gradle.kts
+++ ci/build.gradle.kts
@@ -70,3 +70,11 @@ android {
 flutter {
     source = "../.."
 }
+
+android.applicationVariants.configureEach {
+    val appLabel = "Unitec-Forca-Vendas"
+    outputs.configureEach {
+        val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+        output.outputFileName = "$appLabel-${versionName}.apk"
+    }
+}
